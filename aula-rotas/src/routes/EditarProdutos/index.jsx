b9a2001b6f0432525151633a21222db2036532d8
Str1nder <rm552238@fiap.com.br>
2023-09-06 15:33:02 -0300

    Passando valores pela URL

--- aula-rotas/src/routes/EditarProdutos/index.jsx
+++ aula-rotas/src/routes/EditarProdutos/index.jsx
@@ -1,7 +1,25 @@
+import { useParams, useNavigate} from "react-router-dom"
+import { listaProdutos } from "../../listaProdutos"
+
 export default function EditarProduto(){
+
+    const lista = listaProdutos
+    const navegacao = useNavigate()
+    const {id} = useParams()
+
+    const proc = lista.filter(prod => prod.id == id)
+    const produto = proc[0]
+
+    const salvar = ()=>{
+        alert(`Produto: ${produto.nome} editado com sucesso!`)
+        return nevegacao ('/produtos')
+    }
+
     return(
         <main>
             <h1>Editando o produto</h1>
+            <p>Editando os dados do produto: {produto.nome}</p>
+            <button onClick={salvar}>Salvar</button>
         </main>
     )
 }
